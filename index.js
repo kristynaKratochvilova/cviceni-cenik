f2d516f92013a6d317579265d3846d6a876428e2
kristynaKratochvilova <kristyna.kratochvilova@gmail.com>
2025-03-31 10:47:40 +0200

    Druhý pokus index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const plan1 = document.querySelector ('#plan1');
+const plan2 = document.querySelector ('#plan2');
+const plan3 = document.querySelector ('#plan3');
+
 const selectPlan = (planNumber) => {
+    if (plan1.classList.contains ('plan--selected')) {
+        plan1.classList.remove ('plan--selected');
+    }
+    if (plan2.classList.contains ('plan--selected')) {
+        plan2.classList.remove ('plan--selected');
+    }
+    if (plan3.classList.contains ('plan--selected')) {
+        plan3.classList.remove ('plan--selected');
+    }
+
     const selectPlanNumberElm = document.getElementById (`plan${planNumber}`);
     selectPlanNumberElm.classList.add ('plan--selected');
 };
 
-selectPlan (3)
+selectPlan (3);
